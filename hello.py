139cd2d4d737b6a3df2e6132ee574fe60812e026
khang <machgiakhang@gmail.com>
2026-05-09 08:48:43 -0400

    com1-rename
    com2

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
 v=[3,4,5,1,2,3]
-l:set[int] = set()
+l =[]
 for i in range(4):
-    if i%2 ==0 and l.__contains__(v[i]):
-        l.add(v[i]*10)
+    if i%2 ==0 and not l.__contains__(v[i]):
+        l.append(v[i])
 print(l)
+
+# com1 com3-merge com2-merge 
+# com2 com2-pow com3-chic 
+# com3 com3-pow com3-lamp
