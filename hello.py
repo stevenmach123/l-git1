bc3cff8de19f5ef26f6a1a7ffcab9b4a058a3241
khang <machgiakhang@gmail.com>
2026-07-22 22:34:22 -0400

    condition2 of c1

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 v=[3,4,5,1,2,3]
-l:set[int] = set()
+l:list[int] = []
 
 for i in range(4):
     if i%2 !=0 or l.__contains__(v[i]):
-        l.add(v[i]*100)
-print(l)
+        l.append(v[i]*200)
+print(l+1)
 print(v)
